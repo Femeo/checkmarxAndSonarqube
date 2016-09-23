9bd33369763b0936e9ea772969eddec25f4e3e23
feme.adepetun.dwp <feme.adepetun.dwp@gmail.com>
2016-09-23 09:38:36 +0100

    able to sql inject

---
+++ db.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root'
+})
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -6,23 +6,11 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root'
+    password: 'root',
+    database: 'checkmarxusers'
 })
 
-connection.connect(function(err){
-    if(err){
-        console.log('error connecting: ' + err.stack);
-        return ;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-    connection.query('use checkmarxusers;');
-    connection.query('SELECT * from users;', function(error, result){
-        console.log(result[0]);
-        console.log("error: " + error);
-
-    });
-});
+connection.connect();
 
 
 app.use(bodyParser.urlencoded({
@@ -34,40 +22,34 @@ app.use(bodyParser.json());
 app.use('/', express.static('public'));
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.get('/availableUsernames', function(req, res){
-    usernames = ["root"];
-    res.send(usernames)
-});
-
-app.get('/availablePasswords', function(req, res){
-    passwords = ["pa55w0rd"];
-    res.send(passwords);
-})
-
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Working on port 3000");
 })
 
-app.post('/login', function(req, res){
+app.post('/login', function (req, res) {
     var users = [];
-    connection.query('SELECT username, pass_word from users;', function(error, resultSet){
+    connection.query('INSERT INTO users (username, pass_word) VALUES (\'' + req.body.username + '\' , \'' + req.body.password + '\');', function(err){
+        connection.query('SELECT username, pass_word from users;', function (error, resultSet) {
         var count = 0;
-        for(user in resultSet){
-            if(req.body.username == resultSet[user].username && req.body.password == resultSet[user].pass_word){
+        for (user in resultSet) {
+            if (req.body.username == resultSet[user].username && req.body.password == resultSet[user].pass_word) {
                 res.sendFile(__dirname + "/success.html");
-                return ;
-            } else{
-                count ++;
+                return;
+            } else {
+                count++;
             }
-            if(count == resultSet.length){
+            if (count == resultSet.length) {
                 res.sendFile(__dirname + "/index.html");
             }
-        }       
+        }
     })
+});
+
+
 })
 
 module.exports = app;
